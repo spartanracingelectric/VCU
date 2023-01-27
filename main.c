78800442e0c92a82e308314e0c3ddd76f80cd4cc
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2023-01-26 20:28:18 -0800

    TPS/BPS Changes
    Changed TPS/BPS Values for Driver Needs

--- main.c
+++ main.c
@@ -223,9 +223,9 @@ void main(void)
     // ubyte2 tps1_calibMin = 0x5432;  //me->tps1->sensorValue;
     // ubyte2 tps1_calibMax = 0xCDEF;  //me->tps1->sensorValue;
     ubyte2 tps0_calibMin = 850;  //me->tps0->sensorValue;
-    ubyte2 tps0_calibMax = 1650; //me->tps0->sensorValue;
-    ubyte2 tps1_calibMin = 3270; //me->tps1->sensorValue;
-    ubyte2 tps1_calibMax = 4300; //me->tps1->sensorValue;
+    ubyte2 tps0_calibMax = 2000; //me->tps0->sensorValue;
+    ubyte2 tps1_calibMin = 2700; //me->tps1->sensorValue;
+    ubyte2 tps1_calibMax = 4800; //me->tps1->sensorValue;
     //TODO: Read calibration data from EEPROM?
     //TODO: Run calibration functions?
     //TODO: Power-on error checking?
