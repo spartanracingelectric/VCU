3cf3e0b17c9aca78356f7f6610ba6044dd9f15bb
TheEppicJR <mantacast4154@gmail.com>
2023-11-07 15:59:18 -0800

    fixed CYCLE_TIME

--- dev/main.h
+++ dev/main.h
@@ -1,7 +1,7 @@
 #include <IO_Driver.h>
 
 #define CYCLE_TIME_US 10000 // in microseconds
-#define CYCLE_TIME (100000.0 / CYCLE_TIME_US) // in seconds
+#define CYCLE_TIME (1000000.0 / CYCLE_TIME_US) // in seconds
 #define CAN_0_BAUD 500 // in kbps
 #define CAN_1_BAUD 500 // in kbps
 //16 bumps per rotation, 16 hz = 1 rotation per second
