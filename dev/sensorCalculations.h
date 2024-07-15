4d9f32f9b5488d42777d0ace3596423bd975275d
Mehul Williams <mehulwilliams@gmail.com>
2024-07-14 23:20:19 -0700

    Revert "7/14/24: PL testing  (#171)" (#173)

--- dev/sensorCalculations.h
+++ dev/sensorCalculations.h
@@ -100,7 +100,7 @@ void GetBrakePosition(void);
 //4 funcs, one to get each wheel's speed
 //16 bumps per rotation, 16 hz = 1 rotation per second
 #define NUM_BUMPS 16
-#define WHEEL_DIAMETER 18 //Inches    /// change to 18 check it with DAQ
+#define WHEEL_DIAMETER 16 //Inches
 #define NUM_BUMPS_D (double)NUM_BUMPS
 #define WHEEL_DIAMETER_D (double)WHEEL_DIAMETER
 
