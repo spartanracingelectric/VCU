1ff179952fef46431810ea9e67af0b96ca2f72da
sghillie <145189248+sghillie@users.noreply.github.com>
2024-08-22 23:20:51 -0700

    the random newlines were indeed random. Outcome: reduced randomness

--- dev/powerLimit.h
+++ dev/powerLimit.h
@@ -34,10 +34,8 @@ typedef struct _PowerLimit {
 
 } PowerLimit;
 
-
 void powerLimitTorqueCalculation(TorqueEncoder* tps, MotorController* mcm, PowerLimit* me, BatteryManagementSystem *bms, WheelSpeeds* ws, PID* pid);
 void populatePLHashTable(HashTable* table);
-
 float getTorque(PowerLimit* me, HashTable* torqueHashtable, float noLoadVoltage, float rpm);
 PowerLimit* PL_new(); 
 
