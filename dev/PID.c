ccfcfb9de7db9badd089824dfd4a7572980df50d
sghillie <145189248+sghillie@users.noreply.github.com>
2024-08-27 15:24:35 -0700

    the code looks nice but definitely has issues

--- dev/PID.c
+++ dev/PID.c
@@ -51,6 +51,6 @@ float PID_compute(PID *pid, float sensorValue) {
     float integral     = (float)(pid->Ki * (pid->totalError + currentError * pid->dt));
     float derivative   = (float)(pid->Kd * (currentError - pid->previousError) / pid->dt);
     pid->previousError = currentError;
-    pid->totalError   += currentError * 0.01;
+    pid->totalError   += currentError * pid->dt;
     return proportional + integral + derivative;
 }
