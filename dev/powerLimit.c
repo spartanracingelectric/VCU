580591152ab801e9a064a61cfdee35d9b803b7be
sghillie <145189248+sghillie@users.noreply.github.com>
2024-09-16 18:39:53 -0700

    minor addditions

--- dev/powerLimit.c
+++ dev/powerLimit.c
@@ -117,9 +117,9 @@ void PL_calculateTorqueOffset(TorqueEncoder* tps, MotorController* mcm, PowerLim
     if(me->watts > PL_INIT) {
         me-> plStatus = TRUE;
         ubyte2 maxTQ       = MCM_getTorqueMax(mcm);
-        sbyte2 commandedTorque = MCM_commands_PL_getTorque(me);
-        me->offset = PID_computeOffset(pid, me->watts);
-        ubyte2 offsetTQ   = commandedTorque * (1 + maxTQ ((ubyte2)(me->offset / me->watts * 100)));
+        sbyte2 commandedTQ = MCM_commands_PL_getTorque(me);
+        me->offset         = PID_computeOffset(pid, me->watts);
+        ubyte2 offsetTQ    = commandedTQ * (1 + maxTQ ((ubyte2)(me->offset / me->watts * 100))); //offsetTQ is the complete torque request
         MCM_updateTorqueOffset(mcm, offsetTQ);
     }
     else {
