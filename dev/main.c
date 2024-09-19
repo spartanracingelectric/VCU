d2564501c620ffeaf7bec6cad91a9faae6c968f9
sghillie <145189248+sghillie@users.noreply.github.com>
2024-09-18 18:31:40 -0700

    Adding 80KWH setpoint to plPID initialization

--- dev/main.c
+++ dev/main.c
@@ -224,7 +224,7 @@ void main(void)
 
     DRS *drs = DRS_new();
     PowerLimit *pl = PL_new(); 
-    PID *plPID = PID_new(1.0,0.0,0.0,0.0);
+    PID *plPID = PID_new(1.0,0.0,0.0,80000.0);
 
     //----------------------------------------------------------------------------
     // TODO: Additional Initial Power-up functions
