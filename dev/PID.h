74db1c8e716bfa3dba701270f675b0bd7a4851fe
sghillie <145189248+sghillie@users.noreply.github.com>
2024-11-02 12:04:55 -0700

    Partial

---
+++ dev/PID.h
@@ -0,0 +1,42 @@
+/*****************************************************************************
+ * pid.h - Proportional-Integral-Derivative (PID) controller
+ * Initial Author: Harleen Sandhu / Mehul Williams
+ * Additional Author: Shaun Gilmore
+ ******************************************************************************
+ * General purpose PID controller, initially designed for Torque Vectoring.
+ ****************************************************************************/
+
+#ifndef _PID_H
+#define _PID_H
+
+#include "IO_Driver.h" //Includes datatypes, constants, etc - should be included in every c file
+
+// Define a structure for the PID controller
+typedef struct _PID {
+    sbyte1 Kp;               // Proportional gain
+    sbyte1 Ki;               // Integral     gain
+    sbyte1 Kd;               // Derivative   gain
+    sbyte2 setpoint;         // Target       value
+    sbyte4 previousError;
+    sbyte4 totalError;
+    sbyte1 dH;               // Time interval between PID updates in seconds (VCU tick speed)
+    sbyte2 output;
+}PID;
+
+/* Kp, Ki, & Kd are in deci- units, meaning PID_new(10,0,0,500) gives a Kp of 1.0 and a setpoint of 500 */
+PID* PID_new(sbyte1 Kp, sbyte1 Ki, sbyte1 Kd, sbyte2 setpoint);
+void PID_setTotalError(PID* pid, sbyte4 totalError);
+void PID_updateSetpoint(PID *pid, sbyte2 setpoint);
+void PID_updateGainValues(PID* pid, sbyte1 Kp, sbyte1 Ki, sbyte1 Kd);
+sbyte2 PID_computeOutput(PID *pid, ubyte2 sensorValue);
+
+/** GETTER FUNCTIONS **/
+
+sbyte1 PID_getKp(PID *pid);
+sbyte1 PID_getKi(PID *pid);
+sbyte1 PID_getKd(PID *pid);
+sbyte2 PID_getSetpoint(PID *pid);
+sbyte4 PID_getTotalError(PID* pid);
+sbyte2 PID_getOutput(PID* pid);
+
+#endif //_PID_H
