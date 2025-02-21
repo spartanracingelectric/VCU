1b1690256cb9e71113634779830853ad2d665216
sghillie <145189248+sghillie@users.noreply.github.com>
2025-02-20 18:25:07 -0800

    Adding AssistiveDRS & moving Auto DRS fully into switch case in DRS_update() (#182)
    * code structure for assistive DRS
    
    * Auto to Assistive
    
    * Auto to Assistive
    
    * Update drs.c
    
    * carlie chris code combined
    
    * Setting up timer for assistive drs
    
    * Update drs.c
    
    * code structure for assistive DRS
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    * Removed AssistiveDRS and AutoDRS separate functions, moved all code into appropiate DRS_update() switch case(s), Added in Timers to Assistive ONLY for now, reorganized DRS code, cleaned up loose ends, and added 1 previously unused flag (AutoDRSActive) to CAN - not sure why this would be necessary on a moving car tbh but we'll use it for easy debugging on stands w/out needing DRS solenoid
    
    * Changing AutoDRSActive to be set to False by Default
    
    * code structure for assistive DRS
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    code structure for assistive DRS
    
    Update drs.c
    
    Auto to Assistive
    
    Auto to Assistive
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    * Removed AssistiveDRS and AutoDRS separate functions, moved all code into appropiate DRS_update() switch case(s), Added in Timers to Assistive ONLY for now, reorganized DRS code, cleaned up loose ends, and added 1 previously unused flag (AutoDRSActive) to CAN - not sure why this would be necessary on a moving car tbh but we'll use it for easy debugging on stands w/out needing DRS solenoid
    
    Changing AutoDRSActive to be set to False by Default
    
    * Updating Github Workflow to work on v4 artifact actions, v3 is now depreciated as of January 30, 2025 (#183)
    
    * code structure for assistive DRS
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    code structure for assistive DRS
    
    Update drs.c
    
    Auto to Assistive
    
    Auto to Assistive
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    * Removed AssistiveDRS and AutoDRS separate functions, moved all code into appropiate DRS_update() switch case(s), Added in Timers to Assistive ONLY for now, reorganized DRS code, cleaned up loose ends, and added 1 previously unused flag (AutoDRSActive) to CAN - not sure why this would be necessary on a moving car tbh but we'll use it for easy debugging on stands w/out needing DRS solenoid
    
    Changing AutoDRSActive to be set to False by Default
    
    * code structure for assistive DRS
    
    Setting up timer for assistive drs
    
    carlie chris code combined
    
    Update drs.c
    
    * code structure for assistive DRS
    
    * Update drs.c
    
    * Auto to Assistive
    
    * Setting up timer for assistive drs
    
    * carlie chris code combined
    
    * Update drs.c
    
    * Removed AssistiveDRS and AutoDRS separate functions, moved all code into appropiate DRS_update() switch case(s), Added in Timers to Assistive ONLY for now, reorganized DRS code, cleaned up loose ends, and added 1 previously unused flag (AutoDRSActive) to CAN - not sure why this would be necessary on a moving car tbh but we'll use it for easy debugging on stands w/out needing DRS solenoid
    
    ---------
    
    Co-authored-by: Carlie Yem <carlieyem@yahoo.com>
    Co-authored-by: pancakex82 <127472279+pancakex82@users.noreply.github.com>

--- dev/drs.h
+++ dev/drs.h
@@ -12,19 +12,20 @@
 //      Mode 0 - Always off
 //      Mode 1 - Always on
 //      Mode 2 - Driver controlled
-//      Mode 3 - Auto (sensor controlled)
-enum { STAY_CLOSED, STAY_OPEN, MANUAL, AUTO };
+//      Mode 3 - Assistive (manual & sensor controlled)
+enum { STAY_CLOSED, STAY_OPEN, MANUAL, ASSISTIVE, AUTO };
 
 //_DRS is a structure tag, used to reference the structure for future initializations 
 typedef struct _DRS
 {
     //flag indicating if DRS should be active or inactive based on inputs and mode
-    bool AutoDRSActive;
+    bool AutoDRSActive; //used for debugging drs behavior on CAN
 
     //enum indicating current DRS mode selected based on rotaryswitch
     ubyte1 currentDRSMode;
     ubyte1 buttonPressed;
-    ubyte1 drsFlap;
+    ubyte1 drsFlapOpen;
+    ubyte4 drsSafteyTimer;
 
 } DRS;
 //DRS is an instance of the _DRS struct
@@ -35,14 +36,11 @@ DRS *DRS_new();
 
 //DRS control logic
 void DRS_update(DRS *me, MotorController *mcm, TorqueEncoder *tps, BrakePressureSensor *bps);
-//make sensor comparisons to thresholds and set drsActive flag
-void runAuto(DRS *me, MotorController *mcm, TorqueEncoder *tps, BrakePressureSensor *bps);
-
 //actuate the wing to physically activate DRS
 void DRS_open(DRS *me);
 //retract the wing to physically deactivate DRS
 void DRS_close(DRS *me);
-
-void update_knob(DRS *me);
+// Checks for updates to Rotary Knob Position
+void DRS_selectMode(DRS *me);
 
 #endif
