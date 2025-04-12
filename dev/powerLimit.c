8a9d7068ec5f29d66213ef696dae9bd2ad58d1e0
Akash Karthik <akashkarthik473@gmail.com>
2025-04-12 12:22:01 -0700

    run 2 pid(10,0,0) clamping method 1, targetpower 50, threshold 45, plalways on

--- dev/powerLimit.c
+++ dev/powerLimit.c
@@ -39,7 +39,7 @@ PowerLimit* POWERLIMIT_new(){
     me->plTorqueCommand = 0; 
     me->plTargetPower = 60;// HERE IS WHERE YOU CHANGE POWERLIMIT
     me->plKwLimit = 50; // this is lit never used, dont even touch this
-    me->plInitializationThreshold = me->plTargetPower-15;
+    me->plInitializationThreshold = me->plTargetPower-5;
     me->clampingMethod = 1;
     //LUT Corners
     me->vFloorRFloor = 0;
@@ -64,7 +64,7 @@ void POWERLIMIT_setLimpModeOverride(PowerLimit* me){
 /** COMPUTATIONS **/
 
 void PowerLimit_calculateCommand(PowerLimit *me, MotorController *mcm){
-    me->plInitializationThreshold = me->plTargetPower-15;
+    me->plInitializationThreshold = me->plTargetPower-5;
 
     if (!me->plStatus) 
     {
