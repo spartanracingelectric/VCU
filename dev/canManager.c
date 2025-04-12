240a1d942a8112df806287e56a9a22579644cd09
carlieyem <carlieyem@yahoo.com>
2025-04-11 23:29:30 -0700

    Motor direction and software bspd

--- dev/canManager.c
+++ dev/canManager.c
@@ -817,7 +817,7 @@ void canOutput_sendDebugMessage(CanManager* me, TorqueEncoder* tps, BrakePressur
     canMessages[canMessageCount - 1].data[byteNum++] = MCM_commands_getTorque(mcm) >> 8;
     canMessages[canMessageCount - 1].data[byteNum++] = 0;  //Speed (RPM?) - not needed - mcu should be in torque mode
     canMessages[canMessageCount - 1].data[byteNum++] = 0;  //Speed (RPM?) - not needed - mcu should be in torque mode
-    canMessages[canMessageCount - 1].data[byteNum++] = MCM_commands_getDirection(mcm);  //Motor direction (0 = Reverse, 1 = Forward)
+    canMessages[canMessageCount - 1].data[byteNum++] = 0;  //Motor direction (0 = Reverse, 1 = Forward)
     canMessages[canMessageCount - 1].data[byteNum++] = MCM_commands_getInverterAndSpeedMode(mcm); //unused/unused/unused/unused unused/unused/Discharge/Inverter Enable //Need to use 'last' unused for speed command override
     canMessages[canMessageCount - 1].data[byteNum++] = (ubyte1)MCM_commands_getTorqueLimit(mcm);
     canMessages[canMessageCount - 1].data[byteNum++] = MCM_commands_getTorqueLimit(mcm) >> 8;
