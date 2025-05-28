bc3d1e26d39fd79620c8238132849e58c6d26c0c
Your Name <you@example.com>
2025-05-28 15:04:09 -0700

    run5

--- dev/LaunchControl.c
+++ dev/LaunchControl.c
@@ -31,7 +31,7 @@ static const ubyte1 LC_belowSlipTarget = 0x40;
 static const ubyte1 LC_aboveSlipTarget = 0x80;
 
 //Initial Torque Setpoints
-static const sbyte2 PnR_noAero = 60;
+static const sbyte2 PnR_noAero = 70;
 static const sbyte2 Crows_15Aero = 100;
 
 
@@ -39,6 +39,8 @@ static const sbyte2 Crows_15Aero = 100;
 // PnR MCM_getMotorRPM(mcm) / 3
 // Crows MCM_getMotorRPM(mcm)
 
+// Slip Targets
+// PnR -> ???
 LaunchControl *LaunchControl_new(){
     LaunchControl* me = (LaunchControl*)malloc(sizeof(struct _LaunchControl));
     // malloc returns NULL if it fails to allocate memory
@@ -47,7 +49,7 @@ LaunchControl *LaunchControl_new(){
 
     //Torque Mode Settings for LC
     me->pidTorque = PID_new(2, 0, 0, 0, 1); //No saturation point to see what the behavior of the PID is, will need a saturation value somewhere to prevent wind-up of the pid in the future
-    PID_updateSettings(me->pidTorque, setpoint, 0); // Having a statically coded slip ratio may not be the best. this requires knowing that this is both a) the best slip ratio for the track, and b) that our fronts are not in any way slipping / entirely truthful regarding the groundspeed of the car. Using accel as a target is perhaps better, but needs to be better understood.
+    PID_updateSettings(me->pidTorque, setpoint, 50); // Having a statically coded slip ratio may not be the best. this requires knowing that this is both a) the best slip ratio for the track, and b) that our fronts are not in any way slipping / entirely truthful regarding the groundspeed of the car. Using accel as a target is perhaps better, but needs to be better understood.
     PID_updateSettings(me->pidTorque, frequency, 1);
     me->lcTorqueCommand = NULL;
     me->initialTorque = PnR_noAero;
@@ -198,7 +200,7 @@ void LaunchControl_checkState(LaunchControl *me, TorqueEncoder *tps, BrakePressu
 }
 
 void LaunchControl_initialTorqueCurve(LaunchControl* me, MotorController* mcm){
-    me->lcTorqueCommand = (sbyte2) me->initialTorque + ( MCM_getMotorRPM(mcm) / 4 ); // Tunable Values will be the inital Torque Request @ 0 and the scalar factor
+    me->lcTorqueCommand = (sbyte2) me->initialTorque + ( MCM_getMotorRPM(mcm) / 2 ); // Tunable Values will be the inital Torque Request @ 0 and the scalar factor
 }
 
 void LaunchControl_initialRPMCurve(LaunchControl* me, MotorController* mcm){
